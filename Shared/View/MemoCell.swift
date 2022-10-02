05a96c6b48a74f30933a92e7c0c3facdc0c700c6
daeun <sung980524@naver.com>
2022-10-02 21:12:26 +0900

    step4

---
+++ Shared/View/MemoCell.swift
@@ -0,0 +1,27 @@
+//
+//  MemoCell.swift
+//  swiftMemo
+//
+//  Created by daeun on 2022/10/02.
+//
+
+import SwiftUI
+
+struct MemoCell: View {
+    @ObservedObject var memo : Memo
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text(memo.content)
+                .font(.body).lineLimit(1)
+            Text(memo.insertDate , style: .date)
+                .font(.caption)
+                .foregroundColor(.secondary)
+        }
+    }
+}
+
+struct MemoCell_Previews: PreviewProvider {
+    static var previews: some View {
+        MemoCell(memo: Memo(content: "Text"))
+    }
+}
