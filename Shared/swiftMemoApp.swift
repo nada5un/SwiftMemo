1d66a4b6fdc9d81ec0a30ad61f1db945947369f4
daeun <sung980524@naver.com>
2022-10-02 20:39:43 +0900

    step1

--- Shared/swiftMemoApp.swift
+++ Shared/swiftMemoApp.swift
@@ -13,7 +13,7 @@ struct swiftMemoApp: App {
 
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            MainListView()
                 .environment(\.managedObjectContext, persistenceController.container.viewContext)
         }
     }
